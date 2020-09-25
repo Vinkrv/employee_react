767d39ae7a6c5d32de471820e9c94006dcd7f3d9
Ivan Vinokurov <viv204@yandex.ru>
2020-09-25 11:15:44 +0700

    Updates

---
+++ precache-manifest.9166ec789e1fd539e96ab801cc5e943f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3e4e9bf286210feec9ece269a93e065",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13aed82761b16d1b7c36",
+    "url": "/static/js/2.d003b0a4.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/static/js/2.d003b0a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea79a1f7b5d27e3d55c7",
+    "url": "/static/js/main.00d9da1b.chunk.js"
+  },
+  {
+    "revision": "0d397ed1f46a27fee53f",
+    "url": "/static/js/runtime-main.0f521568.js"
+  }
+]);
